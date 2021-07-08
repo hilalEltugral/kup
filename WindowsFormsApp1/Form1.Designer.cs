fd07c7e0dcfb05b59ff5130e08492538ca249075
hilalEltugral <hilal.eltugral28@gmail.com>
2021-07-08 12:10:06 +0300

    all

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.OpenGlControl1 = new Tao.Platform.Windows.SimpleOpenGlControl();
+            this.button1 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // OpenGlControl1
+            // 
+            this.OpenGlControl1.AccumBits = ((byte)(0));
+            this.OpenGlControl1.AutoCheckErrors = false;
+            this.OpenGlControl1.AutoFinish = false;
+            this.OpenGlControl1.AutoMakeCurrent = true;
+            this.OpenGlControl1.AutoSwapBuffers = true;
+            this.OpenGlControl1.BackColor = System.Drawing.Color.Black;
+            this.OpenGlControl1.ColorBits = ((byte)(32));
+            this.OpenGlControl1.DepthBits = ((byte)(16));
+            this.OpenGlControl1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.OpenGlControl1.Location = new System.Drawing.Point(0, 0);
+            this.OpenGlControl1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.OpenGlControl1.Name = "OpenGlControl1";
+            this.OpenGlControl1.Size = new System.Drawing.Size(310, 373);
+            this.OpenGlControl1.StencilBits = ((byte)(0));
+            this.OpenGlControl1.TabIndex = 0;
+            this.OpenGlControl1.Paint += new System.Windows.Forms.PaintEventHandler(this.OpenGlControl1_Paint);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(380, 134);
+            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(116, 38);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Simülasyonu Başlat";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(585, 373);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.OpenGlControl1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.OpenGlControl1_Paint);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Tao.Platform.Windows.SimpleOpenGlControl OpenGlControl1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+//Tao Gl kütüphaneleri
+using Tao.OpenGl;
+using Tao.Platform.Windows;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        float rotX = 0;
+        float rotY = 0;
+        float rotZ = 0;
+        
+ 
+        public Form1()
+        {
+            InitializeComponent();
+            OpenGlControl1.InitializeContexts();
+            //OpenGl ilk işlemler
+            Gl.glClearColor(0.0f, 0.0f, 0.5f, 0.0f);//ekrana siyah rengi veriyoruz silince
+            Gl.glMatrixMode(Gl.GL_PROJECTION);
+            Gl.glLoadIdentity();
+            Gl.glOrtho(-2.0, 2.0, -2.0, 2.0, -2.0, 2.0);//projeksiyon görüntüsü
+            Gl.glMatrixMode(Gl.GL_MODELVIEW);//modele aktardık
+
+            Gl.glEnable(Gl.GL_DEPTH_TEST);
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+        float[][] pt = {
+        new float[]{-1,-1,-1},new float[]{-1,1,-1},
+        new float[]{1,-1,-1},new float[]{1,1,-1},
+        new float[]{-1,-1,1},new float[]{-1,1,1},
+        new float[]{1,-1,1},new float[]{1,1,1}
+
+        };
+        public void quad(int n1,int n2,int n3,int n4)
+        {
+            Gl.glBegin(Gl.GL_QUADS);
+                Gl.glColor3fv(pt[n1]); Gl.glVertex3fv(pt[n1]);
+                Gl.glColor3fv(pt[n2]); Gl.glVertex3fv(pt[n2]);
+                Gl.glColor3fv(pt[n3]); Gl.glVertex3fv(pt[n3]);
+                Gl.glColor3fv(pt[n4]); Gl.glVertex3fv(pt[n4]);
+            Gl.glEnd();
+        }
+        public void cube()
+        {
+            quad(6, 2, 3, 7); quad(5, 1, 0, 4);
+            quad(7, 3, 1, 5); quad(4, 0, 2, 6);
+            quad(2, 0, 1, 3); quad(7, 5, 4, 6);
+        }
+        private void OpenGlControl1_Paint(object sender, PaintEventArgs e)
+        {         
+            Gl.glClear(Gl.GL_COLOR_BUFFER_BIT | Gl.GL_DEPTH_BUFFER_BIT);
+            Gl.glShadeModel(Gl.GL_SMOOTH);
+
+            Gl.glPushMatrix();
+                Gl.glRotatef(rotX, 1, 0, 0);
+                Gl.glRotatef(rotY, 0, 1, 0);
+                Gl.glRotatef(rotZ, 0, 0, 1);
+                cube();
+            Gl.glPopMatrix();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+              timer1.Start();
+        }    
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            
+            rotX = (rotX + 5) % 360;
+            //  OpenGlControl1.Refresh();
+            rotY = (rotY + 5) % 360;
+            // OpenGlControl1.Refresh();
+            rotZ = (rotZ + 5) % 360;
+            OpenGlControl1.Refresh();
+        }
+    }
+}
